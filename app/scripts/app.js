29a046a2f66a4492013cc41266aa2c8a68c866da
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-01-20 22:38:53 +0100

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('johnsMusicPage', ['ui.bootstrap'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
