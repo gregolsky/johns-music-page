9c1eea2dc02b514be47a5658c181e523bfa5e07c
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-02-11 23:10:00 +0100

    fix: html files in subdirectories should be taken into account when usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,7 +155,7 @@ module.exports = function (grunt) {
       }
     },
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         dirs: ['<%= yeoman.dist %>']
