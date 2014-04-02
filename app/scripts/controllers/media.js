5b74d2f72e495e70cfa4d40975d5a9a7c5fd36e6
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-04-02 21:25:29 +0200

    fix: usemin fix for image references in js files

--- app/scripts/controllers/media.js
+++ app/scripts/controllers/media.js
@@ -6,10 +6,10 @@ angular.module('johnsMusicPage')
         function ($scope, $location, Picture) {
             
             $scope.popduo = [
-                new Picture("Marcin Janek", "images/Janek4.jpg"),
-                new Picture("Michał Bąk", "images/bak4.jpg"),
-                new Picture("Arkadiusz Krawiel", "images/Krawiel2.jpg"),
-                new Picture("? Prosty", "images/Prosty2.jpg"),
+                new Picture('Marcin Janek', 'images/Janek4.jpg'),
+                new Picture('Michał Bąk', 'images/bak4.jpg'),
+                new Picture('Arkadiusz Krawiel', 'images/Krawiel2.jpg'),
+                new Picture('? Prosty', 'images/Prosty2.jpg'),
             ];
             
         }]);
