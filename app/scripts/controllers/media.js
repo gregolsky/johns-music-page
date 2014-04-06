027405444332a3af399749a7cbbb500d96641d28
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-04-06 21:43:34 +0200

    fix: image file renames, media carousels edits

--- app/scripts/controllers/media.js
+++ app/scripts/controllers/media.js
@@ -5,11 +5,16 @@ angular.module('johnsMusicPage')
                     'Picture',
         function ($scope, $location, Picture) {
             
+            $scope.trio = [
+                new Picture('Marcin Janek', 'images/standards-sax.jpg'),
+                new Picture('Michał Bąk', 'images/standards-bass.jpg'),
+                new Picture('Arkadiusz Krawiel', 'images/standards-piano.jpg'),
+                new Picture('? Prosty', 'images/standards-drums.jpg'),
+            ];
+
             $scope.popduo = [
-                new Picture('Marcin Janek', 'images/Janek4.jpg'),
-                new Picture('Michał Bąk', 'images/bak4.jpg'),
-                new Picture('Arkadiusz Krawiel', 'images/Krawiel2.jpg'),
-                new Picture('? Prosty', 'images/Prosty2.jpg'),
+                new Picture('Marcin Janek', 'images/popduo-sax.jpg'),
+                new Picture('Irek Pestka', 'images/popduo-guitar.jpg')
             ];
             
         }]);
