42b834de0d5b9d819ef9d85f77227a8c10d44009
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2015-06-29 23:15:31 +0200

    fix: partially fixed animate scrolling to href

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,20 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+$(function () {
+
+    $("nav a[href^=#]").on('click', function(event) {
+
+        var hashIdx = this.href.indexOf('#');
+        var target = $(this.href.slice(hashIdx));
+
+        if( target.length ) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+
+    });
+
+});
