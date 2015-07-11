d567cb8cd521b8c298b18f60063d508c232b486d
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2015-07-11 14:38:00 +0200

    js fixes and cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,42 @@
 /* jshint devel:true */
 
-(function ($) {
+(function (window, $) {
     'use strict';
 
-    function initializeGalleries() {
-        $('#trio-gallery').carousel();
-        $('#quartet-gallery').carousel();
+    $(window).on('scroll', function () {
+        $('.debug').html(
+            'Page Y Offset: ' + window.pageYOffset + '<br/>' +
+            '#home top: ' + document.getElementById('home').getBoundingClientRect().top + '<br/>' +
+            'calc top: ' + getScrollTop(document.getElementById('home')));
+    });
 
+    function initializeGalleries() {
+        $('#popduo-gallery,#trio-gallery,#string-art-ladies-gallery,#quartet-gallery').carousel();
+    }
+
+    function getScrollTop(target) {
+        return window.pageYOffset + target.getBoundingClientRect().top;
     }
 
     function initializeAnchorLinks() {
-        $('nav a[href^=#]').on('click', function(event) {
+        $('nav a[href^=#], #start a[href^=#]').on('click', function(event) {
 
             var href = this.href;
             var hashIdx = href.indexOf('#');
-            var target = $(href.slice(hashIdx));
+            var target = document.querySelector(href.slice(hashIdx));
 
-            if (target.length) {
+            if (target) {
                 event.preventDefault();
 
-                $('html, body').animate({
-                    scrollTop: target.offset().top
+                $('html body').animate({
+                    scrollTop: getScrollTop(target)
                 }, 1000);
 
                 history.pushState({}, '', href);
+                return;
             }
+
+            throw new Error('Invalid element!');
 
         });
     }
@@ -32,4 +44,4 @@
     initializeGalleries();
     initializeAnchorLinks();
 
-})(window.$);
+})(window, window.$);
